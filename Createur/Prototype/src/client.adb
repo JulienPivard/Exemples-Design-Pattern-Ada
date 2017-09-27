cc16892d670f1252249c48f49fa847f8ef965a44
Ythildyr <pivardjulien@gmail.com>
2017-09-27 22:41:39 +0200

    Prototype terminé

--- Createur/Prototype/src/client.adb
+++ Createur/Prototype/src/client.adb
@@ -4,12 +4,17 @@ with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
 with Ada.Command_Line;
 with Ada.Directories;
 
+with P_Prototype.Mur;   use P_Prototype.Mur;
+with P_Prototype.P_Porte;   use P_Prototype.P_Porte;
+with P_Prototype;   use P_Prototype;
+
 procedure Client is
 
     Nb_Arguments : constant Natural := Ada.Command_Line.Argument_Count;
 
     procedure Afficher_Aide;
 
+    ---------------------------------------------------------------------------
     procedure Afficher_Aide is
     begin
         Put_Line (Standard_Error, "Utilisation du programme :");
@@ -21,6 +26,37 @@ procedure Client is
         Put (Standard_Error, " [arguments]");
         Put_Line (Standard_Error, "");
     end Afficher_Aide;
+    ---------------------------------------------------------------------------
+
+    procedure Test_De_L_Age (P : in T_Prototype'Class);
+
+    ---------------------------------------------------------------------------
+    procedure Test_De_L_Age (P : in T_Prototype'Class) is
+        a : Integer := P.Lire_Age;
+        p_test : T_Prototype'Class := P.Clone;
+    begin
+        Ages :
+        loop
+            p_test := P.Clone;
+            p_test.Changer_Age (a);
+            Ada.Text_IO.Put ("Test de l'age : ");
+            Ada.Text_IO.Put (p_test.Lire_Age'Image);
+            Ada.Text_IO.New_Line (1);
+            exit Ages when p_test.Est_Trop_Vieux;
+            a := a + 10;
+        end loop Ages;
+
+        Ada.Text_IO.Put ("L'age limite est : ");
+        Ada.Text_IO.Put (a'Image);
+        Ada.Text_IO.Put_Line (" ");
+    end Test_De_L_Age;
+    ---------------------------------------------------------------------------
+
+    p1 : T_Mur;
+    p2 : T_Mur;
+
+    po1 : T_Porte;
+    po2 : T_Porte;
 
 begin
 
@@ -46,6 +82,29 @@ begin
     end if;
 
     --  Mettez votre code ici
+    Creer_Mur (p1);
+    p2 := p1.Clone;
+    p2.Changer_Age (20);
+
+    po1.Creer_Porte;
+    po2 := po1.Clone;
+    po2.Changer_Age (30);
+
+    Ada.Text_IO.Put_Line ("------------- Un Mur -------------");
+    Test_De_L_Age (p1);
+    Ada.Text_IO.New_Line (1);
+
+    Ada.Text_IO.Put_Line ("------------- Un Mur -------------");
+    Test_De_L_Age (p2);
+    Ada.Text_IO.New_Line (1);
+
+    Ada.Text_IO.Put_Line ("------------- Une Porte -------------");
+    Test_De_L_Age (po1);
+    Ada.Text_IO.New_Line (1);
+
+    Ada.Text_IO.Put_Line ("------------- Une Porte -------------");
+    Test_De_L_Age (po2);
+    Ada.Text_IO.New_Line (1);
 
     Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
     return;
---
+++ Createur/Prototype/src/p_prototype.ads
@@ -0,0 +1,26 @@
+package P_Prototype is
+
+    --  Le type abstrait dont vont descendre tous les type prototypé
+    type T_Prototype is abstract tagged private;
+
+    --  Permet de créer un clone l'instance.
+    function Clone (Proto : in T_Prototype)
+        return T_Prototype is abstract;
+
+    --  Permet de savoir si le prototype est trop vieux
+    function Est_Trop_Vieux (Proto : in T_Prototype)
+        return Boolean is abstract;
+
+    --  Permet de changer l'age du prototype.
+    procedure Changer_Age (Proto : out T_Prototype; Age : in Integer)
+        is abstract;
+
+    --  Permet de lire l'age du prototype.
+    function Lire_Age (Proto : in T_Prototype)
+        return Integer is abstract;
+
+private
+
+    type T_Prototype is abstract tagged null record;
+
+end P_Prototype;
