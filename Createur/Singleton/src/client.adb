d817191fafc74e3391524043c78b6f7fb7b1cc72
Ythildir <pivardjulien@gmail.com>
2018-09-07 15:21:18 +0200

    Allegement du client de singleton

--- Createur/Singleton/src/client.adb
+++ Createur/Singleton/src/client.adb
@@ -1,38 +1,16 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Dimanche 03 décembre[12] 2017
+--           Dernière modification : Vendredi 07 septembre[09] 2018
 --                                                                          --
 ------------------------------------------------------------------------------
 
 with Ada.Text_IO;
-with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
-
-with Ada.Command_Line;
-with Ada.Directories;
 
 with Singleton_P;
 with Ada.Strings.Unbounded;
 
 procedure Client is
-
-   Nb_Arguments : constant Natural := Ada.Command_Line.Argument_Count;
-
-   ---------------------------------------------------------------------------
-   procedure Afficher_Aide;
-
-   procedure Afficher_Aide is
-   begin
-      Put_Line (Standard_Error, "Utilisation du programme :");
-      Ada.Text_IO.Put
-         (
-            Ada.Text_IO.Standard_Error,
-            Ada.Directories.Base_Name (Ada.Command_Line.Command_Name)
-         );
-      Put (Standard_Error, " [arguments]");
-      Put_Line (Standard_Error, "");
-   end Afficher_Aide;
-   ---------------------------------------------------------------------------
 
    s1 : constant Singleton_P.Singleton_Access_T :=
       Singleton_P.Recuperer_Singleton;
@@ -40,27 +18,6 @@ procedure Client is
       Singleton_P.Recuperer_Singleton;
 
 begin
-
-   if Nb_Arguments = 1 then
-
-      Afficher_Aide;
-      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
-      return;
-
-   elsif Nb_Arguments > 1 then
-
-      Afficher_Aide;
-      Put (Standard_Error, "Trop d'arguments. ");
-      Put_Line (Standard_Error, "Les arguments suivants sont invalide : ");
-      for i in 1 .. Nb_Arguments loop
-         Put (Standard_Error, "  - ");
-         Ada.Text_IO.Put_Line
-            (Ada.Text_IO.Standard_Error, Ada.Command_Line.Argument (i));
-      end loop;
-      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
-      return;
-
-   end if;
 
    Ada.Text_IO.New_Line (1);
    Ada.Text_IO.Put_Line ("====  Après modification de S1  ====");
@@ -79,8 +36,5 @@ begin
       (Ada.Strings.Unbounded.To_Unbounded_String ("Hey poivros"));
    s1.all.Afficher;
    s2.all.Afficher;
-
-   Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
-   return;
 
 end Client;
