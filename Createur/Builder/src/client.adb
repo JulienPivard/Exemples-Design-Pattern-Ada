9cda90c5bf4357754a63b37c2c4265ec9417f821
Ythildir <pivardjulien@gmail.com>
2018-09-07 13:24:31 +0200

    Allegement du client de monteur

--- Createur/Builder/src/client.adb
+++ Createur/Builder/src/client.adb
@@ -1,8 +1,4 @@
 with Ada.Text_IO;
-with Ada.Wide_Wide_Text_IO; use Ada.Wide_Wide_Text_IO;
-
-with Ada.Command_Line;
-with Ada.Directories;
 
 with P_Directeur;
 with P_Builder.P_Builder_Html;
@@ -10,75 +6,35 @@ with P_Produit_Html;
 with P_Builder.P_Builder_Markdown;
 with P_Produit_Markdown;
 
-use  P_Directeur;
-use  P_Builder.P_Builder_Html;
-use  P_Produit_Html;
-use  P_Builder.P_Builder_Markdown;
-use  P_Produit_Markdown;
-
 procedure Client is
 
-   Nb_Arguments : constant Natural := Ada.Command_Line.Argument_Count;
+   package Directeur_P        renames P_Directeur;
+   package Html_B_P           renames P_Builder.P_Builder_Html;
+   package Produit_Html_P     renames P_Produit_Html;
+   package Mark_B_P           renames P_Builder.P_Builder_Markdown;
+   package Produit_Markdown_P renames P_Produit_Markdown;
 
-   dir : T_Directeur;
-   monH : T_Monteur_Html;
-   texH : T_Texte_Html;
-   monM : T_Monteur_Markdown;
-   texM : T_Texte_Markdown;
-
-   procedure Afficher_Aide;
-
-   procedure Afficher_Aide is
-   begin
-      Put_Line (Standard_Error, "Utilisation du programme :");
-      Ada.Text_IO.Put
-      (
-         Ada.Text_IO.Standard_Error,
-         Ada.Directories.Base_Name (Ada.Command_Line.Command_Name)
-      );
-      Put (Standard_Error, " [arguments]");
-      Put_Line (Standard_Error, "");
-   end Afficher_Aide;
+   dir  : Directeur_P.T_Directeur;
+   monH : Html_B_P.T_Monteur_Html;
+   texH : Produit_Html_P.T_Texte_Html;
+   monM : Mark_B_P.T_Monteur_Markdown;
+   texM : Produit_Markdown_P.T_Texte_Markdown;
 
 begin
 
-   if Nb_Arguments = 1 then
-
-      Afficher_Aide;
-      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
-      return;
-
-   elsif Nb_Arguments > 1 then
-
-      Afficher_Aide;
-      Put (Standard_Error, "Trop d'arguments. ");
-      Put_Line (Standard_Error, "Les arguments suivants sont invalide : ");
-      for i in 1 .. Nb_Arguments loop
-         Put (Standard_Error, "  - ");
-         Ada.Text_IO.Put_Line
-            (Ada.Text_IO.Standard_Error, Ada.Command_Line.Argument (i));
-      end loop;
-      Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Failure);
-      return;
-
-   end if;
-
    --  Mettez votre code ici
-   Put_Line ("--------------");
+   Ada.Text_IO.Put_Line ("--------------");
 
    dir.Construire (monH);
    texH := monH.Recuperer_Resultat;
    texH.Rendu;
 
-   Put_Line ("--------------");
+   Ada.Text_IO.Put_Line ("--------------");
 
    dir.Construire (monM);
    texM := monM.Recuperer_Resultat;
    texM.Rendu;
 
-   Put_Line ("--------------");
-
-   Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
-   return;
+   Ada.Text_IO.Put_Line ("--------------");
 
 end Client;
