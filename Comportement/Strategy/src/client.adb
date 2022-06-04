adca9a1004c76eee4436c95694e56e786c6e0d06
Ythildir <pivardjulien@gmail.com>
2022-06-05 00:57:18 +0200

    Ajout d'affichage avec client

--- Comportement/Strategy/src/client.adb
+++ Comportement/Strategy/src/client.adb
@@ -4,12 +4,16 @@
 --           Dernière modification : Dimanche 19 janvier[01] 2020
 --                                                                          --
 ------------------------------------------------------------------------------
-
 with Ada.Text_IO;
+
+with GNAT.Compiler_Version;
+with GNAT.Source_Info;
+
 with Executeur_G;
 
 procedure Client is
 
+   package Version_Compilateur_P is new GNAT.Compiler_Version;
    package Executeur_P is new Executeur_G
       (
          Nombre_D_Arguments_Min => 0,
@@ -17,6 +21,19 @@ procedure Client is
       );
 
 begin
+
+   Ada.Text_IO.Put      (Item => "+---------------------+");
+   Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
+   Ada.Text_IO.Put      (Item => "| Date de compilation :");
+   Ada.Text_IO.Put      (Item => "  ");
+   Ada.Text_IO.Put      (Item => GNAT.Source_Info.Compilation_ISO_Date);
+   Ada.Text_IO.Put      (Item => " ");
+   Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Compilation_Time);
+   Ada.Text_IO.Put      (Item => "+---------------------+");
+   Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
+
+   Ada.Text_IO.Put      (Item => "Version du compilateur : ");
+   Ada.Text_IO.Put_Line (Item => "[" & Version_Compilateur_P.Version & "]");
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
