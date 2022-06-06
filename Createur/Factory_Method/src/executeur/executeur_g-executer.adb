8fe37e895a40db82265e0be691dadbbb7433721e
Ythildir <pivardjulien@gmail.com>
2022-06-06 11:58:23 +0200

    Ajout texte explicatif et pragma

--- Createur/Factory_Method/src/executeur/executeur_g-executer.adb
+++ Createur/Factory_Method/src/executeur/executeur_g-executer.adb
@@ -7,15 +7,13 @@ separate (Executeur_G)
 procedure Executer
    --  (Arguments)
 is
-
    F : Fabrique_P.Concret_P.Concret_T;
    P : Produit_P.Produit_T'Class := F.Fabriquer;
-
 begin
-
+   Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern Fabrique");
+   Ada.Text_IO.Put_Line (Item => "Fabrique un produit.");
    Ada.Text_IO.New_Line (Spacing => 1);
 
    P.Creer_Produit;
    P.Afficher;
-
 end Executer;
