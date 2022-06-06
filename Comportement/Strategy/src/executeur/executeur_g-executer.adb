6c83b5241fa44cbec3ec0f8d26b3e046c105165b
Ythildir <pivardjulien@gmail.com>
2022-06-06 11:43:27 +0200

    Ajout petite explication de la demo

--- Comportement/Strategy/src/executeur/executeur_g-executer.adb
+++ Comportement/Strategy/src/executeur/executeur_g-executer.adb
@@ -12,6 +12,13 @@ is
    S : Strat_R.Strategie_T;
    P : Distance_P.Probleme_T;
 begin
+   Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern stratégie.");
+   Ada.Text_IO.Put_Line (Item => "Calcul de la distance entre deux points.");
+   Ada.Text_IO.Put_Line (Item => "Le problème : deux points dont on veux");
+   Ada.Text_IO.Put_Line (Item => "              connaitre la distance");
+   Ada.Text_IO.Put_Line (Item => "La stratégie : Le calcul de la distance.");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
    P := Distance_P.Initialiser (X1 => 3, Y1 => 3, X2 => 6, Y2 => 6);
 
    P.Resoudre (Strategie => S);
