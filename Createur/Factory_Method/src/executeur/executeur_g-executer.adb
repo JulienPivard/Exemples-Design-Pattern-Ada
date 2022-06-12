4d6ee8b79c6740f39078212143d7710817c65ac3
Ythildir <pivardjulien@gmail.com>
2022-06-12 13:55:16 +0200

    Amélioration d'affichage

--- Createur/Factory_Method/src/executeur/executeur_g-executer.adb
+++ Createur/Factory_Method/src/executeur/executeur_g-executer.adb
@@ -10,8 +10,10 @@ is
    F : Fabrique_P.Concret_P.Concret_T;
    P : Produit_P.Produit_T'Class := F.Fabriquer;
 begin
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern Fabrique");
    Ada.Text_IO.Put_Line (Item => "Fabrique un produit.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.New_Line (Spacing => 1);
 
    P.Creer_Produit;
