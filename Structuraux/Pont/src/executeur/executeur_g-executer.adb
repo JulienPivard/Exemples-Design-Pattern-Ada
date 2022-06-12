3a43983abf0bfde1146619bf1b709f556210e371
Ythildir <pivardjulien@gmail.com>
2022-06-12 10:13:52 +0200

    Ajout d'explications

--- Structuraux/Pont/src/executeur/executeur_g-executer.adb
+++ Structuraux/Pont/src/executeur/executeur_g-executer.adb
@@ -8,7 +8,11 @@ is
    A : Abstraction_P.Abstraction_T;
    F : Abstraction_P.Fenetre_P.Fenetre_T;
 begin
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern pont.");
+   Ada.Text_IO.Put_Line (Item => "Une abstraction avec deux impl différentes");
+   Ada.Text_IO.Put_Line (Item => "et une spécialisation de l'abstraction.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.New_Line (Spacing => 1);
 
    Ada.Text_IO.Put_Line (Item => "Pont Operation 1 sur abstraction");
