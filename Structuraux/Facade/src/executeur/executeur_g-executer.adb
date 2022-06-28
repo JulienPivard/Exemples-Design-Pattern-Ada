3db747932b5e5b37e38ad8d086abf815b58f58cf
Ythildir <pivardjulien@gmail.com>
2022-06-28 17:41:48 +0200

    Ajout explications

--- Structuraux/Facade/src/executeur/executeur_g-executer.adb
+++ Structuraux/Facade/src/executeur/executeur_g-executer.adb
@@ -8,6 +8,8 @@ is
 begin
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern façade.");
+   Ada.Text_IO.Put_Line (Item => "Le but est de cacher la complexité");
+   Ada.Text_IO.Put_Line (Item => "d'utilisation d'objets spécialisé.");
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.New_Line (Spacing => 1);
 
--- Structuraux/Facade/src/facade/facade_p.adb
+++ Structuraux/Facade/src/facade/facade_p.adb
@@ -1,3 +1,5 @@
+with Ada.Text_IO;
+
 package body Facade_P
    with Spark_Mode => Off
 is
@@ -7,7 +9,9 @@ is
       (Facade : in     Facade_T)
    is
    begin
-      null;
+      Ada.Text_IO.Put_Line (Item => "Appel premier objet complexe");
+      Ada.Text_IO.Put_Line (Item => "Appel deuxième objet complexe");
+      Ada.Text_IO.Put_Line (Item => "Appel troisième objet complexe");
    end Executer;
    ---------------------------------------------------------------------------
 
