53dbef26c7d51a202cc4f94536fa5c65c531c25a
Ythildir <pivardjulien@gmail.com>
2022-06-30 16:58:59 +0200

    Mise en place exemple pattern procuration

---
+++ Structuraux/Procuration/src/executeur/executeur_g-executer.adb
@@ -0,0 +1,11 @@
+separate (Executeur_G)
+procedure Executer
+   --  (Arguments)
+is
+begin
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern procuration");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+end Executer;
