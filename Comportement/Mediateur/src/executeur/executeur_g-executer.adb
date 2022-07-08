e35910b2708e916620ade57254fb3753adc01bc8
Ythildir <pivardjulien@gmail.com>
2022-07-08 23:43:26 +0200

    Mise en place exemple médiateur

---
+++ Comportement/Mediateur/src/executeur/executeur_g-executer.adb
@@ -0,0 +1,11 @@
+separate (Executeur_G)
+procedure Executer
+   --  (Arguments)
+is
+begin
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern médiateur.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+end Executer;
