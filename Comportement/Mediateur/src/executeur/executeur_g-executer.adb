521ee03334be5a9f13f5eb84cce124ff01a7c9eb
Ythildir <pivardjulien@gmail.com>
2022-07-11 08:38:13 +0200

    Ajout explication

--- Comportement/Mediateur/src/executeur/executeur_g-executer.adb
+++ Comportement/Mediateur/src/executeur/executeur_g-executer.adb
@@ -15,6 +15,12 @@ begin
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.Put_Line
       (Item => "Démonstration du design pattern médiateur.");
+   Ada.Text_IO.Put_Line (Item => "Le but du médiateur est de permettre à");
+   Ada.Text_IO.Put_Line (Item => "plusieurs classes de travailler ensembles");
+   Ada.Text_IO.Put_Line (Item => "sans se connaitre ni s'appeler directement");
+   Ada.Text_IO.Put_Line (Item => "mais vont passer par le médiateur.");
+   Ada.Text_IO.Put_Line (Item => "Le but est de simplifier les relations");
+   Ada.Text_IO.Put_Line (Item => "inter-classes.");
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.New_Line (Spacing => 1);
 
