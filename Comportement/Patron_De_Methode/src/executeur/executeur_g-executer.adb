08ec4a7009e8f16b223ee5bdcd90d1859045136b
Ythildir <pivardjulien@gmail.com>
2022-07-17 19:47:01 +0200

    Mise en place exemple pattern patron de méthode

---
+++ Comportement/Patron_De_Methode/src/executeur/executeur_g-executer.adb
@@ -0,0 +1,11 @@
+separate (Executeur_G)
+procedure Executer
+   --  (Arguments)
+is
+begin
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern patron de méthode.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+end Executer;
