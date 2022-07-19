6116ced5972c3a293f7347dd71bccb0fa25c2dd2
Ythildir <pivardjulien@gmail.com>
2022-07-19 09:02:21 +0200

    Suppression pointeur inutile

--- Createur/Abstract_Factory/src/produit_a/produit_a_p.ads
+++ Createur/Abstract_Factory/src/produit_a/produit_a_p.ads
@@ -8,9 +8,6 @@ package Produit_A_P is
    type Produit_T is abstract tagged private;
    --  Produit abstrait de type A.
 
-   type Produit_A is access Produit_T'Class;
-   --  Pointeur vers un produit de type A.
-
    procedure Annonce
       (Produit : in     Produit_T)
    is abstract;
