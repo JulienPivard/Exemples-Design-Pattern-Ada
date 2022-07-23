17eda4d4e6a31de23a9bd5696ad3c9f09f0d5bf7
Ythildir <pivardjulien@gmail.com>
2022-07-23 11:04:00 +0200

    Indentation

--- Createur/Singleton_(Singleton)/src/executeur/executeur_g-executer.adb
+++ Createur/Singleton_(Singleton)/src/executeur/executeur_g-executer.adb
@@ -21,27 +21,27 @@ begin
       S2 : constant Singleton_P.Singleton_A :=
          Singleton_P.Recuperer_Singleton;
    begin
-   Ada.Text_IO.New_Line (Spacing => 1);
-   Ada.Text_IO.Put_Line (Item => "====  Après modification de S1  ====");
-   Ada.Text_IO.New_Line (Spacing => 1);
+      Ada.Text_IO.New_Line (Spacing => 1);
+      Ada.Text_IO.Put_Line (Item => "====  Après modification de S1  ====");
+      Ada.Text_IO.New_Line (Spacing => 1);
 
-   S1.all.Changer_Nom (Nom => "Roulecouele");
+      S1.all.Changer_Nom (Nom => "Roulecouele");
 
-   Ada.Text_IO.Put (Item => "S1 : ");
-   S1.all.Afficher;
-   Ada.Text_IO.Put (Item => "S2 : ");
-   S2.all.Afficher;
+      Ada.Text_IO.Put (Item => "S1 : ");
+      S1.all.Afficher;
+      Ada.Text_IO.Put (Item => "S2 : ");
+      S2.all.Afficher;
 
-   Ada.Text_IO.New_Line (Spacing => 1);
-   Ada.Text_IO.Put_Line (Item => "====  Après modification de S2  ====");
-   Ada.Text_IO.New_Line (Spacing => 1);
+      Ada.Text_IO.New_Line (Spacing => 1);
+      Ada.Text_IO.Put_Line (Item => "====  Après modification de S2  ====");
+      Ada.Text_IO.New_Line (Spacing => 1);
 
-   S2.all.Changer_Nom (Nom => "Hey poivros");
+      S2.all.Changer_Nom (Nom => "Hey poivros");
 
-   Ada.Text_IO.Put (Item => "S1 : ");
-   S1.all.Afficher;
-   Ada.Text_IO.Put (Item => "S2 : ");
-   S2.all.Afficher;
+      Ada.Text_IO.Put (Item => "S1 : ");
+      S1.all.Afficher;
+      Ada.Text_IO.Put (Item => "S2 : ");
+      S2.all.Afficher;
    end Bloc_Singleton_Access;
 
    Ada.Text_IO.New_Line (Spacing => 2);
