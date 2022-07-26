9243cf2f141ace6eb890394aec71cba2518b1bc7
Ythildir <pivardjulien@gmail.com>
2022-07-26 21:40:33 +0200

    Création bloc demo access pour mediateur

--- Comportement/Mediateur_(Mediator)/src/executeur/executeur_g-executer.adb
+++ Comportement/Mediateur_(Mediator)/src/executeur/executeur_g-executer.adb
@@ -5,12 +5,6 @@ separate (Executeur_G)
 procedure Executer
    --  (Arguments)
 is
-   U_1 : aliased Utilisateur_P.Collegue_P.Utilisateur_Collegue_T :=
-      Utilisateur_P.Collegue_P.Creer (Nom => "Morgane             ");
-   U_2 : aliased Utilisateur_P.Collegue_P.Utilisateur_Collegue_T :=
-      Utilisateur_P.Collegue_P.Creer (Nom => "Cassandre           ");
-
-   M : aliased Mediateur_P.Concret_P.Concret_T;
 begin
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.Put_Line
@@ -24,11 +18,21 @@ begin
    Ada.Text_IO.Put_Line (Item => "------------------------------------------");
    Ada.Text_IO.New_Line (Spacing => 1);
 
+   Bloc_Demo_Avec_Access :
+   declare
+      U_1 : aliased Utilisateur_P.Collegue_P.Utilisateur_Collegue_T :=
+         Utilisateur_P.Collegue_P.Creer (Nom => "Morgane             ");
+      U_2 : aliased Utilisateur_P.Collegue_P.Utilisateur_Collegue_T :=
+         Utilisateur_P.Collegue_P.Creer (Nom => "Cassandre           ");
+
+      M : aliased Mediateur_P.Concret_P.Concret_T;
+   begin
    M.Inscrire (Utilisateur => U_1'Unchecked_Access);
    M.Inscrire (Utilisateur => U_2'Unchecked_Access);
 
    U_1.Envoyer (Message => "Wesh Cassandre !    ");
    U_2.Envoyer (Message => "Yo Morgane          ");
+   end Bloc_Demo_Avec_Access;
 
    Ada.Text_IO.New_Line (Spacing => 2);
 end Executer;
