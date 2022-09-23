395fca82efe1651fa35df8eb7accfe02385f4fc9
Ythildir <pivardjulien@gmail.com>
2022-09-23 18:46:56 +0200

    Ajout package lettre

---
+++ Comportement/Etat_(State)/src/lettre_p.ads
@@ -0,0 +1,23 @@
+--  @summary
+--  L'énumération des lettres utilisable.
+--  @description
+--  L'énumération des lettres utilisable.
+--  @group Lettres
+package Lettre_P
+   with
+      Pure           => True,
+      Preelaborate   => False,
+      Elaborate_Body => False,
+      Spark_Mode     => Off
+is
+
+   type Id_Touche_T is
+      (
+         Lettre_E,
+         Lettre_I,
+         Lettre_V,
+         Lettre_R,
+         Lettre_Esc
+      );
+
+end Lettre_P;
