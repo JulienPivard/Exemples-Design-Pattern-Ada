ee081896b09daadacd0cb5d69c06bb9d7c3ed310
Ythildir <pivardjulien@gmail.com>
2025-04-03 19:42:56 +0200

    fix ligne trop longue

--- Comportement/Etat_(State)/src/utilisateur/utilisateur_p.adb
+++ Comportement/Etat_(State)/src/utilisateur/utilisateur_p.adb
@@ -63,7 +63,8 @@ is
       if Il_Faut_Changer_D_Etat then
          Bloc_Changer_D_Etat :
          declare
-            Etat : constant Etat_P.Etat_T'Class := This.Transition.Changer_D_Etat
+            Etat : constant Etat_P.Etat_T'Class :=
+               This.Transition.Changer_D_Etat
                   (
                      Etat   => This.Etat.Element,
                      Touche => Touche
