3ea84cdf870cecc41af4299a06faaca02b310c8f
Ythildir <pivardjulien@gmail.com>
2025-09-29 19:55:18 +0200

    Suppression executeur

--- Createur/Fabrique_Abstraite_(Abstract_Factory)/src/client.adb
+++ Createur/Fabrique_Abstraite_(Abstract_Factory)/src/client.adb
@@ -1,26 +1,63 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Fabrique_P;
+with Fabrique_P.Markdown_P;
+with Fabrique_P.Html_P;
+with Produit_A_P;
+with Produit_B_P;
 
 procedure Client is
-
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
+   ---------------------------------------------------------------------------
+   procedure Construire
       (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
+         Fabrique : in     Fabrique_P.Fabrique_T'Class;
+         Titre    : in     String
       );
+   --  Construit les produits à partir de la fabrique.
+   --  @param Fabrique
+   --  La fabrique.
+   --  @param Titre
+   --  Le titre à afficher.
 
+   --------------------
+   procedure Construire
+      (
+         Fabrique : in     Fabrique_P.Fabrique_T'Class;
+         Titre    : in     String
+      )
+   is
+      Produit_A : constant Produit_A_P.Produit_T'Class :=
+         Fabrique.Creer_Produit_A;
+      Produit_B : constant Produit_B_P.Produit_T'Class :=
+         Fabrique.Creer_Produit_B;
+   begin
+      Ada.Text_IO.Put_Line (Item    => Titre);
+      Ada.Text_IO.New_Line (Spacing => 1);
+      Produit_A.Annonce;
+      Produit_B.Presente;
+   end Construire;
+   ---------------------------------------------------------------------------
+
+   package Fab_1_R renames Fabrique_P.Markdown_P;
+   package Fab_2_R renames Fabrique_P.Html_P;
+
+   Fabrique_1 : Fab_1_R.Fabrique_Markdown_T;
+   Fabrique_2 : Fab_2_R.Fabrique_Html_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +74,33 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern Fabrique Abstraite.");
+   Ada.Text_IO.Put_Line (Item => "Une fabrique qui génère des instances");
+   Ada.Text_IO.Put_Line (Item => "concrète d'objets d'une même famille.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line (Item => "----------------------------");
+   Construire (Fabrique => Fabrique_1, Titre => "En markdown : ");
+
+   Ada.Text_IO.Put_Line (Item => "----------------------------");
+
+   Construire (Fabrique => Fabrique_2, Titre => "En html : ");
+   Ada.Text_IO.Put_Line (Item => "----------------------------");
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
