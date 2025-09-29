939b12c078b9262451cc2269030fbcbda72b13fa
Ythildir <pivardjulien@gmail.com>
2025-09-29 18:38:11 +0200

    Maj steucture commande

--- Comportement/Commande_(Command)/src/client.adb
+++ Comportement/Commande_(Command)/src/client.adb
@@ -1,18 +1,38 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Dimanche 17 novembre[11] 2024
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
 with GNAT.Source_Info;
 
-with Executeur_P;
 with Version_Compilateur_P;
 
+with Commande_P;
+with Commande_P.Concrete_P;
+
 procedure Client is
+   ---------------------------------------------------------------------------
+   procedure Executer
+      (Commande : in out Commande_P.Commande_T'Class);
+
+   ----------------
+   procedure Executer
+      (Commande : in out Commande_P.Commande_T'Class)
+   is
+   begin
+      Commande.Execute;
+   end Executer;
+   ---------------------------------------------------------------------------
+
+   C : Commande_P.Concrete_P.Commande_Concrete_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -29,11 +49,31 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   if Executeur_P.Verifier_Nombre_D_Arguments_Est_Valide then
-      Executeur_P.Executer;
-   end if;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
 
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern commande.");
+   Ada.Text_IO.Put_Line (Item => "Très similaire à l'idée de callback dans");
+   Ada.Text_IO.Put_Line (Item => "les interfaces graphiques.");
+   Ada.Text_IO.Put_Line (Item => "On entoure la commande à exécuter pour");
+   Ada.Text_IO.Put_Line (Item => "disposer d'une interface générique, mais");
+   Ada.Text_IO.Put_Line (Item => "qui permet de faire ce que l'on veux");
+   Ada.Text_IO.Put_Line (Item => "derrière.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Executer (Commande => C);
+
+   pragma Unreferenced (C);
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 exception
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
