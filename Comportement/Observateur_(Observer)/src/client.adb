d25cb4d817657285fe97498c7d5805df3bf1f62e
Ythildir <pivardjulien@gmail.com>
2025-09-29 19:37:30 +0200

    Suppression executeur

--- Comportement/Observateur_(Observer)/src/client.adb
+++ Comportement/Observateur_(Observer)/src/client.adb
@@ -1,26 +1,30 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Observateur_Chiffre_P;
+with Observateur_Graphe_P;
+with Valeurs_P.Concret_P;
 
 procedure Client is
+   G : constant Observateur_Graphe_P.Graphe_T   := Observateur_Graphe_P.Creer;
+   C : constant Observateur_Chiffre_P.Chiffre_T := Observateur_Chiffre_P.Creer;
 
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
-      (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
-      );
-
+   V : Valeurs_P.Concret_P.Sujet_T;
+   T : Valeurs_P.Table_Valeurs_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +41,82 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern observateur.");
+   Ada.Text_IO.Put_Line (Item => "Observateur sans pointeur, mais qui");
+   Ada.Text_IO.Put_Line (Item => "nécessite de passer par un singleton,");
+   Ada.Text_IO.Put_Line (Item => "dans les coulisses, pour faire référence");
+   Ada.Text_IO.Put_Line (Item => "à un même objet si il y a besoin.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line (Item => "On attache un observateur de [graphe]");
+   V.Attache (Observateur => C);
+   Ada.Text_IO.Put_Line (Item => "On attache un observateur de [valeurs]");
+   V.Attache (Observateur => G);
+
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line
+      (Item => "  Modification des valeurs de l'objet surveillé");
+   Ada.Text_IO.Put_Line
+      (Item => "  Provoque La mise à jour des observateurs :");
+   T := Valeurs_P.Table_Valeurs_T'
+      (
+         1 => 4,
+         2 => 32,
+         3 => 9,
+         4 => 55,
+         5 => 0
+      );
+   V.Modifier (Vals => T);
+
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line
+      (Item => "  Modification des valeurs de l'objet surveillé");
+   Ada.Text_IO.Put_Line
+      (Item => "  Provoque La mise à jour des observateurs :");
+   T := Valeurs_P.Table_Valeurs_T'
+      (
+         1 => 32,
+         2 => 0,
+         3 => 55,
+         4 => 4,
+         5 => 9
+      );
+   V.Modifier (Vals => T);
+
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line
+      (Item => "  Modification des valeurs de l'objet surveillé");
+   Ada.Text_IO.Put_Line
+      (Item => "  Provoque La mise à jour des observateurs :");
+   T := Valeurs_P.Table_Valeurs_T'
+      (
+         1 => 9,
+         2 => 36,
+         3 => 4,
+         4 => 19,
+         5 => 32
+      );
+   V.Modifier (Vals => T);
+
+   pragma Unreferenced (V);
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
