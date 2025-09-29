6ff1c364356495d14dc00356bca3c8af1b990992
Ythildir <pivardjulien@gmail.com>
2025-09-29 20:03:00 +0200

    Suppression executeur

--- Createur/Singleton_(Singleton)/src/client.adb
+++ Createur/Singleton_(Singleton)/src/client.adb
@@ -1,26 +1,27 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Faire_Action;
+
+with Singleton_P;
+with Singleton_P.Instance_P;
+with Singleton_P.Protege_P;
 
 procedure Client is
-
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
-      (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
-      );
-
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +38,61 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern singleton");
+   Ada.Text_IO.Put_Line (Item => "S1 et S2 sont deux variables différentes ");
+   Ada.Text_IO.Put_Line (Item => "mais qui accèdent au même singleton.");
+   Ada.Text_IO.Put_Line (Item => "Le second exemple avec Si_1 et Si_2 est");
+   Ada.Text_IO.Put_Line (Item => "une version task safe. Les deux");
+   Ada.Text_IO.Put_Line (Item => "objets modifie le même singleton.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Bloc_Singleton_Access :
+   declare
+      S1 : constant Singleton_P.Accesseur_T :=
+         Singleton_P.Instance_P.Recuperer_Singleton;
+      S2 : constant Singleton_P.Accesseur_T :=
+         Singleton_P.Instance_P.Recuperer_Singleton;
+   begin
+      Faire_Action
+         (
+            S1 => S1,
+            S2 => S2
+         );
+   end Bloc_Singleton_Access;
+
+   Ada.Text_IO.New_Line (Spacing => 2);
+   Ada.Text_IO.Put_Line (Item => "===============================");
+   Ada.Text_IO.Put_Line (Item => "====   Version task safe   ====");
+   Ada.Text_IO.Put_Line (Item => "===============================");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Bloc_Singleton_Protege :
+   declare
+      Si_1 : constant Singleton_P.Accesseur_T :=
+         Singleton_P.Protege_P.Recuperer_Singleton;
+      Si_2 : constant Singleton_P.Accesseur_T :=
+         Singleton_P.Protege_P.Recuperer_Singleton;
+   begin
+      Faire_Action
+         (
+            S1 => Si_1,
+            S2 => Si_2
+         );
+   end Bloc_Singleton_Protege;
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
