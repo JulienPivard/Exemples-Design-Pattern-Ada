51d983932a4bfb6d8525a1352763a872284b8198
Ythildir <pivardjulien@gmail.com>
2025-09-29 20:05:34 +0200

    Suppression executeur

--- Structuraux/Adaptateur_(Adapter)/src/client.adb
+++ Structuraux/Adaptateur_(Adapter)/src/client.adb
@@ -1,26 +1,41 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Adaptateur_De_Classe_P;
+with Adaptateur_D_Objet_P;
+
+with Classe_Utilisee_P;
 
 procedure Client is
+   ---------------------------------------------------------------------------
+   procedure Execute
+      (Executrice : in out Classe_Utilisee_P.Classe_Utilisee_T'Class);
 
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
-      (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
-      );
+   -----------------
+   procedure Execute
+      (Executrice : in out Classe_Utilisee_P.Classe_Utilisee_T'Class)
+   is
+   begin
+      Executrice.Faire;
+   end Execute;
+   ---------------------------------------------------------------------------
 
+   A : Adaptateur_De_Classe_P.Adaptateur_T;
+   O : Adaptateur_D_Objet_P.Adaptateur_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +52,37 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern adaptateur.");
+   Ada.Text_IO.Put_Line (Item => "Une version adaptateur de classe, et");
+   Ada.Text_IO.Put_Line (Item => "une version adaptateur d'objet.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line (Item => "--  Exemple avec adaptateur de classe");
+   Ada.Text_IO.New_Line (Spacing => 1);
+   Execute (Executrice => A);
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line (Item => "--  Exemple avec adaptateur d'objets");
+   Ada.Text_IO.New_Line (Spacing => 1);
+   Execute (Executrice => O);
+
+   pragma Unreferenced (A);
+   pragma Unreferenced (O);
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
