345388ef6895d66a6bd6008d7275272bc422d0f4
Ythildir <pivardjulien@gmail.com>
2025-09-29 19:50:23 +0200

    Suppression executeur

--- Createur/Fabrication_(Factory_Method)/src/client.adb
+++ Createur/Fabrication_(Factory_Method)/src/client.adb
@@ -1,26 +1,43 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Fabrique_P.Concret_1_P;
+with Fabrique_P.Concret_2_P;
+with Fabrique_P;
+with Produit_P;
 
 procedure Client is
+   ---------------------------------------------------------------------------
+   procedure Utiliser
+      (Fabrique : in     Fabrique_P.Fabrique_T'Class);
 
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
-      (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
-      );
+   ------------------
+   procedure Utiliser
+      (Fabrique : in     Fabrique_P.Fabrique_T'Class)
+   is
+      P : Produit_P.Produit_T'Class := Fabrique.Fabriquer;
+   begin
+      P.Initialiser;
+      P.Afficher;
+   end Utiliser;
+   ---------------------------------------------------------------------------
 
+   F_1 : Fabrique_P.Concret_1_P.Concret_T;
+   F_2 : Fabrique_P.Concret_2_P.Concret_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +54,30 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line
+      (Item => "Démonstration du design pattern Fabrication.");
+   Ada.Text_IO.Put_Line (Item => "Définit une interface de création d'objets");
+   Ada.Text_IO.Put_Line (Item => "mais laisse à ses filles le soins de");
+   Ada.Text_IO.Put_Line (Item => "créer les instances concrète du produit.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Utiliser (Fabrique => F_1);
+   Ada.Text_IO.New_Line (Spacing => 1);
+   Utiliser (Fabrique => F_2);
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
