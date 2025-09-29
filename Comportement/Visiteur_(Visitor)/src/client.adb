db9fad8c51673e2be196efe2576186d421a4a4d0
Ythildir <pivardjulien@gmail.com>
2025-09-29 19:46:22 +0200

    Suppression executeur

--- Comportement/Visiteur_(Visitor)/src/client.adb
+++ Comportement/Visiteur_(Visitor)/src/client.adb
@@ -1,26 +1,81 @@
 ------------------------------------------------------------------------------
 --                                                                          --
 --                          Auteur : PIVARD Julien                          --
---           Dernière modification : Mercredi 25 mai[05] 2022
+--           Dernière modification : Lundi 29 septembre[09] 2025
 --                                                                          --
 ------------------------------------------------------------------------------
+with Ada.Command_Line;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
-with GNAT.Compiler_Version;
 with GNAT.Source_Info;
 
-with Executeur_G;
+with Version_Compilateur_P;
+
+with Uml_P;
+with Visiteur_P.Code_Generateur_P;
+with Visiteur_P.Instrumentation_P;
+with Visiteur_P.Verificateur_P;
+with Visiteur_P;
 
 procedure Client is
+   ---------------------------------------------------------------------------
+   procedure Execution
+      (Visiteur : in out Visiteur_P.Visiteur_T'Class);
 
-   package Version_Compilateur_P is new GNAT.Compiler_Version;
-   package Executeur_P           is new Executeur_G
-      (
-         Nombre_D_Arguments_Min => 0,
-         Nombre_D_Arguments_Max => 0
-      );
+   ------------------
+   procedure Execution
+      (Visiteur : in out Visiteur_P.Visiteur_T'Class)
+   is
+      Var   : Uml_P.Element_Nomme_T;    --  Un visité
+      Class : Uml_P.Code_Class_T;       --  Un visité
+      Pack  : Uml_P.Verif_Package_T;    --  Un visité
+      Ope   : Uml_P.Operation_T;        --  Un visité
+   begin
+      Pack.Visiter (Le_Visiteur => Visiteur);
 
+      Class.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Class.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Class.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Ope.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Ope.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+
+      Pack.Visiter (Le_Visiteur => Visiteur);
+
+      Pack.Visiter (Le_Visiteur => Visiteur);
+
+      Class.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+
+      Var.Visiter (Le_Visiteur => Visiteur);
+      Ope.Visiter (Le_Visiteur => Visiteur);
+      Var.Visiter (Le_Visiteur => Visiteur);
+
+      Pack.Visiter (Le_Visiteur => Visiteur);
+
+      pragma Unreferenced (Var);
+      pragma Unreferenced (Class);
+      pragma Unreferenced (Pack);
+      pragma Unreferenced (Ope);
+   end Execution;
+   ---------------------------------------------------------------------------
+
+   Gen  : Visiteur_P.Code_Generateur_P.Code_Generateur_T;  --  Un visiteur
+   Ver  : Visiteur_P.Verificateur_P.Verificateur_T;        --  Un visiteur
+   Inst : Visiteur_P.Instrumentation_P.Instrumentation_T;
 begin
+   Ada.Command_Line.Set_Exit_Status
+      (Code => Ada.Command_Line.Success);
 
    Ada.Text_IO.Put      (Item => "+---------------------+");
    Ada.Text_IO.Put_Line (Item => " - - - - - - - - - - - ");
@@ -37,17 +92,38 @@ begin
 
    Ada.Text_IO.New_Line (Spacing => 1);
 
-   Executeur_P.Verifier_Nombre_D_Arguments;
-   Executeur_P.Executer;
+   --  Ada.Text_IO.Put      (Item => "Procédure : [");
+   --  Ada.Text_IO.Put      (Item => GNAT.Source_Info.Enclosing_Entity);
+   --  Ada.Text_IO.Put      (Item => "], une instance de : ");
+   --  Ada.Text_IO.Put_Line (Item => GNAT.Source_Info.Source_Location);
+
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.Put_Line (Item => "Démonstration du design pattern visiteur");
+   Ada.Text_IO.Put_Line (Item => "sur une hiérarchie d'objets.");
+   Ada.Text_IO.Put_Line (Item => "------------------------------------------");
+   Ada.Text_IO.New_Line (Spacing => 1);
+
+   Ada.Text_IO.Put_Line (Item => "======  Visiteur générateur ======");
+   Execution (Visiteur => Gen);
+   Ada.Text_IO.New_Line (Spacing => 1);
+   Ada.Text_IO.Put_Line (Item => "======  Visiteur vérification ======");
+   Execution (Visiteur => Ver);
+   Ada.Text_IO.New_Line (Spacing => 1);
+   Ada.Text_IO.Put_Line (Item => "======  Visiteur instrumentation ======");
+   Execution (Visiteur => Inst);
+
+   Inst.Afficher;
+
+   pragma Unreferenced (Gen);
+   pragma Unreferenced (Ver);
+   pragma Unreferenced (Inst);
+
+   Ada.Text_IO.New_Line (Spacing => 2);
 
 exception
-   when Executeur_P.Trop_D_Arguments_E =>
-      null;
-   when Executeur_P.Pas_Assez_D_Arguments_E =>
-      null;
-   when Executeur_P.Option_Incorrect_E =>
-      null;
-   when Executeur_P.Valeur_Option_Incorrect_E =>
-      null;
-
+   when E : others =>
+      Ada.Text_IO.Put_Line
+         (Item => Ada.Exceptions.Exception_Information (X => E));
+      Ada.Command_Line.Set_Exit_Status
+         (Code => Ada.Command_Line.Failure);
 end Client;
